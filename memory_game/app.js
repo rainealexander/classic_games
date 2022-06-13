36a8641f53555a46d291a1cb9878926e9924ce35
taadams1 <rainedoescode@gmail.com>
2022-06-12 20:59:27 -0500

    Added images and created card array

--- memory_game/app.js
+++ memory_game/app.js
@@ -1,8 +1,55 @@
+// card array with two of each image
 const cardArray = [
   {
+    name: 'birdfoot',
+    image: 'assets/birdfoot.png'
+  },
+  {
     name: 'daybloom',
-    image: './assets/daybloom.png'
+    image: 'assets/daybloom.png'
+  },
+  {
+    name: 'frostlily',
+    image: 'assets/frostlily.png'
+  },
+  {
+    name: 'madroot',
+    image: 'assets/madroot.png'
+  },
+  {
+    name: 'moonflower',
+    image: 'assets/moonflower.png'
+  },
+  {
+    name: 'thornroot',
+    image: 'assets/thornroot.png'
+  },{
+    name: 'birdfoot',
+    image: 'assets/birdfoot.png'
+  },
+  {
+    name: 'daybloom',
+    image: 'assets/daybloom.png'
+  },
+  {
+    name: 'frostlily',
+    image: 'assets/frostlily.png'
+  },
+  {
+    name: 'madroot',
+    image: 'assets/madroot.png'
+  },
+  {
+    name: 'moonflower',
+    image: 'assets/moonflower.png'
+  },
+  {
+    name: 'thornroot',
+    image: 'assets/thornroot.png'
   }
-]
+];
+
+// randomly sort array
+cardArray.sort(() => Math.random() - 0.5);
 
 const cardGrid = document.getElementById('card-grid');
